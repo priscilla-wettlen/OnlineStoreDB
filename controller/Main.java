75bd0ec154c2356fd37e6ac8626af4aacdc49f2e
Priscilla Silva <priscilla.sg91@gmail.com>
2024-12-19 15:20:24 +0100

    managed to create query to insert customer data

--- controller/Main.java
+++ controller/Main.java
@@ -1,7 +1,17 @@
 package controller;
 
+import model.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main{
-    public static void main(String[] args){
-        Controller theController = new Controller();
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Starting the application...");
+        DBConnection db = new DBConnection();
+        Connection conn = db.testConnection();
+        db.insertCustomer(conn, "customer", 2, "Priscilla", "Wettlen", "priscilla@email.com", "Huvudgatan", "Malmö","Sverige","666","abc123" );
+        System.out.println("Application finished.");
     }
+
 }
---
+++ model/Customer.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Customer {
+    private DBConnection dbConnection;
+
+    public Customer() {
+        dbConnection = new DBConnection();
+    }
+}
